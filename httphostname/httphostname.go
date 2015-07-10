eac134f0153e2027456b1426fe847079989f076c
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-07-10 21:27:47 +0200

    Include line break in HTTP response

--- httphostname/httphostname.go
+++ httphostname/httphostname.go
@@ -15,6 +15,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		log.Print(req.Method, req.URL)
 		io.WriteString(w, hostname)
+		io.WriteString(w, "\n")
 	})
 	log.Printf("Serving on \"%s:8080\"...", hostname)
 	err = http.ListenAndServe(":8080", nil)
